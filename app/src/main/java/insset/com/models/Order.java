7700859841ce8f57926c50fe8704652f9dbc88b3
PierrePav <p.pavone59@gmail.com>
2016-10-18 10:55:26 +0200

    ajout liste des orders

---
+++ app/src/main/java/insset/com/models/Order.java
@@ -0,0 +1,35 @@
+package insset.com.models;
+
+/**
+ * Created by pierre on 18/10/2016.
+ */
+public class Order {
+
+    int id = 0;
+    Pizza pizza;
+    String status = "";
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Pizza getPizza() {
+        return pizza;
+    }
+
+    public void setPizza(Pizza pizza) {
+        this.pizza = pizza;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ app/src/main/java/insset/com/models/Pizza.java
@@ -0,0 +1,34 @@
+package insset.com.models;
+
+/**
+ * Created by pierre on 18/10/2016.
+ */
+public class Pizza {
+
+    int id = 0, price = 0;
+    String name = "";
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
