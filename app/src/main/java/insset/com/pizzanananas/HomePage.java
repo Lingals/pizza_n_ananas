f976864ab3cfb970fb8241a5168e771beb18f6a3
equipe1 <equipe1@hackardennes.com>
2016-10-18 10:55:34 +0200

    Add ClientPage

--- app/src/main/java/insset/com/pizzanananas/HomePage.java
+++ app/src/main/java/insset/com/pizzanananas/HomePage.java
@@ -1,13 +1,43 @@
 package insset.com.pizzanananas;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
+
+    private Button home_page_client_b,
+            home_page_admin_b;
+
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        context = this;
+
+        initializeFields();
+    }
+
+    public void initializeFields() {
+        home_page_client_b = (Button) findViewById(R.id.home_page_client_b);
+        home_page_admin_b = (Button) findViewById(R.id.home_page_admin_b);
+    }
+
+    public void goAction(View view) {
+        Intent i = new Intent(getApplicationContext(), ClientPage.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void goAdmin(View view) {
+        /*Intent i = new Intent(getApplicationContext(), .class);
+        startActivity(i);
+        finish();*/
     }
 }
