8826c8b08be030dd1b453b06315d8a90ffc43c0f
PierrePav <p.pavone59@gmail.com>
2016-10-18 10:16:02 +0200

    first commit

---
+++ app/src/main/java/insset/com/pizzanananas/MainActivity.java
@@ -0,0 +1,72 @@
+package insset.com.pizzanananas;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
+import insset.com.utils.Constant;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressDialog progressDialog;
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        context = this;
+
+        getPizzas();
+    }
+
+    private void getPizzas() {
+        AsyncHttpClient client = new AsyncHttpClient();
+        //RequestParams params = new RequestParams();
+
+        client.addHeader("Authorization", Constant.Authorization);
+
+
+        client.setMaxRetriesAndTimeout(2, 3000);
+
+        JsonHttpResponseHandler responseHandler = new JsonHttpResponseHandler() {
+
+
+            public void onSuccess(int statusCode,Header[] headers,org.json.JSONArray response) {
+                System.out.println("Success");
+                Log.e("Je vois", "La reponse" + response.toString());
+                if (progressDialog.isShowing())
+                    progressDialog.dismiss();
+
+            }
+
+            public void onFailure(int statusCode,Header[] headers, Throwable throwable,	org.json.JSONObject response) {
+                System.out.println("Failure Json");
+                Log.e("Je vois", "La reponse" + response.toString());
+                if (progressDialog.isShowing())
+                    progressDialog.dismiss();
+            }
+
+            public void onFailure(int statusCode,Header[] headers,String result, Throwable throwable) {
+                System.out.println("Failure String");
+                Log.e("Status Code", statusCode+"");
+                Log.e("Je vois", "La reponse" + result.toString());
+                if (progressDialog.isShowing())
+                    progressDialog.dismiss();
+
+            }
+        };
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setMessage("Récupération des pizzas");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+        client.get(Constant.host + Constant.getPizzas, responseHandler);
+    }
+}
---
+++ app/src/main/java/insset/com/utils/Constant.java
@@ -0,0 +1,11 @@
+package insset.com.utils;
+
+/**
+ * Created by pierre on 18/10/2016.
+ */
+public class Constant {
+
+    public static final String host = "http://pizzapi.herokuapp.com";
+    public static final String getPizzas = "/pizzas";
+    public static final String Authorization = "pizzanananas";
+}
